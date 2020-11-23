3303f78a5797de81170d24b052a39cfefd2824c1
Tiffany Conrad <tiffany.conrad@smu.ca>
2020-11-22 23:52:38 -0400

    Added an Accordion component to use on Help Page

---
+++ client/pages/components/Accordion.js
@@ -0,0 +1,32 @@
+import React, {useState, useRef} from 'react';
+import styles from "../../styles/accordion.module.css";
+export default function Accordion(props){
+    const [setActive, setActiveState] = useState("");
+    const [setHeight, setHeightState] = useState("0px");
+
+    const content = useRef(null);
+
+    function toggleAccordion(){
+        setActiveState(setActive === "" ? "active" : "");
+        setHeightState(setActive === "active" ? "0px" : `${content.current.scrollHeight}px`);
+    }
+
+    return(
+        <div className = {styles.accordionSection}>
+            <button className={`button is-info is-medium is-fullwidth ${setActive}`} onClick={toggleAccordion}>
+                <p className={styles.accordionTitle}>{props.title}</p>
+            </button>
+        
+        <div
+        ref={content}
+        style={{maxHeight: `${setHeight}`}}
+        className={styles.accordionContent}
+        >
+        <div
+            className={styles.accordionText}
+            dangerouslySetInnerHTML={{__html: props.content}}
+            />
+            </div>
+            </div>
+    );
+}
