a821042c7e9fb02e9f5f254df4b2a0706d7839c9
Tiffany Conrad <tiffany.conrad@smu.ca>
2020-11-23 20:27:01 -0400

    Formatted

--- client/pages/components/Accordion.js
+++ client/pages/components/Accordion.js
@@ -4,41 +4,46 @@
 
  @author Tiffany Conrad (A00414194)
  */
-import React, {useState, useRef} from 'react';
+import React, { useState, useRef } from "react";
 import styles from "../../styles/accordion.module.css";
 
 /**
  * Makes the accordion
  * @author Tiffany Conrad (A00414194)
- * @param props 
+ * @param props
  */
-export default function Accordion(props){
-    const [setActive, setActiveState] = useState("");
-    const [setHeight, setHeightState] = useState("0px");
+export default function Accordion(props) {
+  const [setActive, setActiveState] = useState("");
+  const [setHeight, setHeightState] = useState("0px");
 
-    const content = useRef(null);
+  const content = useRef(null);
 
-    function toggleAccordion(){
-        setActiveState(setActive === "" ? "active" : "");
-        setHeightState(setActive === "active" ? "0px" : `${content.current.scrollHeight}px`);
-    }
-
-    return(
-        <div className = {styles.accordionSection}>
-            <button className={`button is-info is-medium is-fullwidth ${setActive}`} onClick={toggleAccordion}>
-                <p className={styles.accordionTitle}>{props.title}</p>
-            </button>
-        
-        <div
-        ref={content}
-        style={{maxHeight: `${setHeight}`}}
-        className={styles.accordionContent}
-        >
-        <div
-            className={styles.accordionText}
-            dangerouslySetInnerHTML={{__html: props.content}}
-            />
-            </div>
-            </div>
+  function toggleAccordion() {
+    setActiveState(setActive === "" ? "active" : "");
+    setHeightState(
+      setActive === "active" ? "0px" : `${content.current.scrollHeight}px`
     );
+  }
+
+  return (
+    <div className={styles.accordionSection}>
+      <button
+        className={`button is-info is-medium is-fullwidth ${setActive}`}
+        onClick={toggleAccordion}
+      >
+        <p className={styles.accordionTitle}>{props.title}</p>
+      </button>
+
+      <div
+        ref={content}
+        style={{ maxHeight: `${setHeight}` }}
+        className={styles.accordionContent}
+      >
+        <div
+          className={styles.accordionText}
+          dangerouslySetInnerHTML={{ __html: props.content }}
+        />
+      </div>
+    </div>
+  );
 }
