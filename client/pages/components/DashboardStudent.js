9210fe4e23214b5beda085ec165073ea24f6a8b2
just1ngray <just1ngray@outlook.com>
2020-11-30 15:40:08 -0400

    implement specialist dashboard

---
+++ client/pages/components/DashboardStudent.js
@@ -0,0 +1,42 @@
+/**
+ * An action row to browse and delete a managed student
+ * @author Justin Gray (A00426753) - the whole file
+ */
+import React from "react";
+
+export default function DashboardStudent({ student, remove }) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        borderBottom: "1px solid black",
+        borderTop: "1px solid black",
+        padding: "10px",
+        backgroundColor: "#d9d9d9",
+      }}
+    >
+      <div style={{ display: "flex", width: "100%", alignItems: "center" }}>
+        <div style={{ flexGrow: 1 }}>{student.account.name}</div>
+        <div style={{ flexGrow: 1 }}>{student.account.email}</div>
+      </div>
+      <div>
+        <button
+          type="button"
+          className="button is-danger"
+          onClick={() => {
+            if (
+              window.confirm(
+                `Do you really want to delete the student account ${student.account.name}?`
+              )
+            ) {
+              remove();
+            }
+          }}
+        >
+          X
+        </button>
+      </div>
+    </div>
+  );
+}
