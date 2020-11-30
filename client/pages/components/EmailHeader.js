490ee071374a29940f46d27b9fb4be4a131949d7
just1ngray <just1ngray@outlook.com>
2020-11-29 22:02:57 -0400

    prettier reformat + clarity improvements

--- client/pages/components/EmailHeader.js
+++ client/pages/components/EmailHeader.js
@@ -8,36 +8,31 @@ import React from "react";
 import { useRouter } from "next/router";
 import * as Formatter from "../../utils/formatter";
 
-export default function EmailHeader({ userAuthor, email }) {
-    const router = useRouter();
+export default function EmailHeader({ isViewerSender, email }) {
+  const router = useRouter();
 
-    let author = "";
-    if (userAuthor && email) author = Formatter.contacts(email.to)
-    if (email) author = Formatter.contact(email.from);
+  let author = "";
+  if (isViewerSender && email) author = Formatter.contacts(email.to);
+  if (email) author = Formatter.contact(email.from);
 
-    function formatSubject() {
-        const sub = email ? email.subject : "";
-        if(sub.length >= 20){
-            return sub.substring(0, 20) + "...";
-        }
-        return sub;
+  function formatSubject() {
+    const sub = email ? email.subject : "";
+    if (sub.length >= 20) {
+      return sub.substring(0, 20) + "...";
     }
+    return sub;
+  }
 
-    function handleClick() {
-        router.push(`/ViewEmail`); //todo dynamic route
-        // localStorage.setItem('emailID', email._id);
-        localStorage.setItem('emailData', JSON.stringify(email));
-        // store.dispatch({
-        //     type: "userAuthor",
-        //     payload: {userAuthor}
-        // })
-    }
+  function handleClick() {
+    router.push(`/ViewEmail`);
+    localStorage.setItem("emailData", JSON.stringify(email));
+  }
 
-    return (
-        <div onClick={handleClick}>
-            <span>{author}</span>
-            <span>{formatSubject()}</span>
-            <span></span>
-        </div>
-    );
+  return (
+    <div onClick={handleClick}>
+      <span>{author}</span>
+      <span>{formatSubject()}</span>
+      <span></span>
+    </div>
+  );
 }
