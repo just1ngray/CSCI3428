d72fcfd88c51825cd5bc77bf65e4ace9609ad57d
just1ngray <just1ngray@outlook.com>
2020-11-29 22:03:07 -0400

    streamlining

--- client/pages/components/EmailList.js
+++ client/pages/components/EmailList.js
@@ -2,7 +2,7 @@
  * This file displays a list of EmailHeader components in Sent Items and Inbox
  *
  * @author Nicholas Morash (A00378981)
- * @author Bivash ... 
+ * @author Bivash ...
  */
 
 import React, { useState, useEffect } from "react";
@@ -11,39 +11,87 @@ import EmailHeader from "./EmailHeader";
 import defaults from "../../utils/defaults";
 
 /**
- * This function populates the list of emails.
- * @param obj {object containin JWT data}
- * @param isSent {boolean, true if emails are sent mails}
+ * This component retrieves and renders a list of emails.
+ * @props isSentPage    boolean, true if emails are sent mails
  */
-export default function EmailList({userAuthor}) {
+export default function EmailList({ isSentPage }) {
   const [emails, setEmails] = useState([]);
-  const [token, setToken] = useState("");
+  const [searchVal, setSearchVal] = useState("");
 
+  // get all the emails
   useEffect(() => {
-    const t = localStorage.getItem("token");
-    setToken(t);
-    
-    axios.get(`${defaults.serverUrl}/email/${userAuthor ? "sent" : "inbox"}`, {
-      headers: { "x-auth-token": t },
-    })
-      .then(res => {
-        setEmails(res.data);
-        console.log(res.data);
+    const token = localStorage.getItem("token");
+
+    axios
+      .get(`${defaults.serverUrl}/email/${isSentPage ? "sent" : "inbox"}`, {
+        headers: { "x-auth-token": token },
       })
-      .catch(err => console.error(err));
+      .then((res) => {
+        const data = res.data.map((eml) => {
+          return { ...eml, isViewing: true };
+        });
+        setEmails(data);
+      })
+      .catch((err) => {
+        console.error(err);
+        setEmails(null);
+      });
   }, []);
 
-  const content = [];
-  emails.forEach(email => {
-    console.log(email);
-    content.push(<EmailHeader key={email._id} email={email} userAuthor={userAuthor} />);
-  });
+  // automatically runs when the searchVal is changed (and maybe first render?)
+  useEffect(() => {
+    const copy = emails; // todo: implement deep clone
+
+    if (searchVal.length === 0) {
+      // show all
+      return;
+    }
+
+    // based on searchVal, change emails[x].isViewing property
+  }, [searchVal]);
+
+  // OR you may choose to have a button call processSearch
+  function processSearch() {}
+
+  let content;
+  if (!emails) content = <strong>Error: Could not retrieve emails</strong>;
+  else if (emails.length == 0)
+    content = <strong>You have no emails in your mailbox!</strong>;
+  else {
+    content = [];
+    emails
+      .filter((email) => email.isViewing === true)
+      .forEach((email) => {
+        content.push(
+          <EmailHeader
+            key={email._id}
+            email={email}
+            isViewerSender={isSentPage}
+          />
+        );
+      });
+  }
 
   return (
-    <div className="box">
-      <p>
-        {content.length > 0 ? content : <strong>You have no emails in your mailbox!</strong>}
-      </p>
+    <div>
+      <input
+        value={searchVal}
+        onChange={(e) => setSearchVal(e.target.value)}
+        placeholder="Search todo"
+      />
+
+      <h4 className="card-title">
+        <span>
+          <u>{isSentPage ? "To" : "From"}</u>
+        </span>
+        <span className="float-right">
+          <u>SUBJECT</u>
+        </span>
+      </h4>
+
+      <div className="box">
+        <p>{content}</p>
+      </div>
     </div>
   );
 }
