d532671bb0826036c357bc7a69418ea43da87c62
just1ngray <just1ngray@outlook.com>
2020-12-03 15:27:40 -0400

    simplified... feel free to revert this commit :)

--- client/pages/components/EmailList.js
+++ client/pages/components/EmailList.js
@@ -6,7 +6,7 @@
  * @author Bivash Pandey (A00425523) - search bar and its filter functionality
  */
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import axios from "axios";
 import EmailHeader from "./EmailHeader";
 import defaults from "../../utils/defaults";
@@ -17,10 +17,12 @@ import defaults from "../../utils/defaults";
  */
 export default function EmailListOld({ isSentPage }) {
   const [emails, setEmails] = useState([]);
+  const [showEmailIndices, setShowEmailIndices] = useState([]);
   const [searchVal, setSearchVal] = useState("");
-  let completeEmail = [];
-  const matchedIndex = [];
-  let filteredEmails = [];
+
+  useEffect(() => {
+    setShowEmailIndices(getIndices(emails, searchVal));
+  }, [searchVal]);
 
   // get all the emails
   useEffect(() => {
@@ -31,11 +33,8 @@ export default function EmailListOld({ isSentPage }) {
         headers: { "x-auth-token": token },
       })
       .then((res) => {
-        const data = res.data.map((eml) => {
-          return { ...eml, isViewing: true };
-        });
-        setEmails(data);
-        console.log(data);
+        setEmails(res.data);
+        setShowEmailIndices([...new Array(res.data.length).keys()]);
       })
       .catch((err) => {
         console.error(err);
@@ -43,45 +42,16 @@ export default function EmailListOld({ isSentPage }) {
       });
   }, []);
 
-  // loop through the data received from database and store receivers detail in tempArray
-  // store concatenation of subject and body in completeEmail array
-  let tempArray = [];
-  emails.forEach((element) => {
-    completeEmail.push(element.subject + " " + element.body);
-    tempArray.push(element["to"]);
-  });
-
-  // loop through tempArray and replace completeEmail's data by adding name and email at front
-  let i = 0;
-  tempArray.forEach((element) => {
-    element.forEach((e) => {
-      completeEmail[i] = e.name + " " + e.email + " " + completeEmail[i];
-    });
-    i += 1;
-  });
-
-  // loop through completeEmail array and find the index of matched word with the user's search
-  // if there is a match, then store that index in matchedIndex array
-  for (let j = 0; j < completeEmail.length; j++) {
-    if (completeEmail[j].toLowerCase().includes(searchVal.toLowerCase())) {
-      matchedIndex.push(j);
-    }
-  }
-
-  // loop through matchedIndex array and filter the data that was received from database
-  // store those filtered data in filteredEmails which is later used to populate the list
-  for (let k = 0; k < matchedIndex.length; k++) {
-    filteredEmails.push(emails[matchedIndex[k]]);
-  }
-
   let content;
   if (!emails) content = <strong>Error: Could not retrieve emails</strong>;
   else if (emails.length == 0)
     content = <strong>You have no emails in your mailbox!</strong>;
+  else if (showEmailIndices.length == 0)
+    content = <strong>No emails match your search</strong>;
   else {
     content = [];
-    filteredEmails
-      .filter((email) => email.isViewing === true)
+    emails
+      .filter((email, index) => showEmailIndices.includes(index))
       .forEach((email) => {
         content.push(
           <EmailHeader
@@ -121,4 +91,48 @@ export default function EmailListOld({ isSentPage }) {
       </div>
     </div>
   );
+}
+
+/**
+ * Find the indices of emails matching a search term
+ * @param emails    array of emails to search through and reference the index of
+ * @param searchVal the value we are searching for in the emails array
+ * @returns         an array of indices that match the search term
+ */
+function getIndices(emails, searchVal) {
+  // if no search term, then render all
+  if (searchVal.length === 0) {
+    return [...Array(emails.length).keys()];
+  }
+
+  const indices = [];
+
+  emails.forEach((email, index) => {
+    // subject search
+    if (email.subject.toLowerCase().includes(searchVal.toLowerCase())) {
+      indices.push(index);
+      return;
+    }
+
+    // body search
+    if (email.body.toLowerCase().includes(searchVal.toLowerCase())) {
+      indices.push(index);
+      return;
+    }
+
+    // address search
+    const addresses = [
+      email.from.email,
+      ...email.to.map((to) => to.email),
+      ...email.cc.map((cc) => cc.email),
+    ];
+    for (const addr of addresses) {
+      if (addr.toLowerCase().includes(searchVal.toLowerCase())) {
+        indices.push(index);
+        return;
+      }
+    }
+  });
+
+  return indices;
 }
