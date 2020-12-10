c01a82dd12a2724cd8a24daad88433add0eca2f5
just1ngray <just1ngray@outlook.com>
2020-12-10 12:32:15 -0400

    addressbook handles all the state

--- client/pages/AddressBook.js
+++ client/pages/AddressBook.js
@@ -2,21 +2,33 @@
  * This is a Address Book Page for creating and displaying contacts
  * Users are not able to create a contact unless both name and email is entered
  *
- * Bivash Pandey (A00425523)
+ * @author Bivash Pandey (A00425523)
+ * @author Justin Gray (A00426753) - added delete option & state improvements
  */
-import React, { useState } from "react";
-import store from "../store";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import defaults from "../utils/defaults";
 import PageTitle from "./components/PageTitle";
 import Layout from "./components/Layout";
 import CustomButton from "./components/CustomButton";
-import ContactList from "./components/ContactList";
+import ContactList from "./components/ContactRow";
 
 export default function AddressBook() {
-  const [data, setData] = useState([]);
+  const [contacts, setContacts] = useState([]);
   const [name, setName] = useState(null);
   const [email, setEmail] = useState(null);
+
+  useEffect(() => {
+    const jwt = localStorage.getItem("token");
+    axios
+      .get(`${defaults.serverUrl}/account/contacts`, {
+        headers: {
+          "x-auth-token": jwt,
+        },
+      })
+      .then((res) => setContacts(res.data))
+      .catch((err) => console.error(err));
+  }, []);
 
   // These variables are for checking if the Name and Email is valid
   const isValidName =
@@ -40,13 +52,41 @@ export default function AddressBook() {
         },
       })
       .then((res) => {
-        setData(res.data); // {name,email,_id}
+        const cloneContacts = JSON.parse(JSON.stringify(contacts));
+        cloneContacts.push(res.data);
+        setContacts(cloneContacts);
       })
-      .catch((err) => {})
+      .catch((err) => {
+        console.error(err);
+      })
       .finally(() => {
         // clear the input boxes
         setName("");
         setEmail("");
+      });
+  }
+
+  /**
+   * Deletes a contact from the local state of the application
+   * @param {string} email the email of the contact
+   * @author Justin Gray
+   */
+  function deleteContact(email) {
+    const jwt = localStorage.getItem("token");
+    axios
+      .delete(`${defaults.serverUrl}/account/contact/${email}`, {
+        headers: {
+          "x-auth-token": jwt,
+        },
+      })
+      .then(() => {
+        const cloneContacts = JSON.parse(JSON.stringify(contacts)).filter(
+          (c) => c.email !== email
+        );
+        setContacts(cloneContacts);
+      })
+      .catch((err) => {
+        alert("Could not delete contact\n" + err);
       });
   }
 
@@ -93,7 +133,24 @@ export default function AddressBook() {
         </div>
         <div>
           <br />
-          <ContactList contact={data} />
+          <table className="table is-bordered is-hoverable is-fullwidth">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {contacts.map((c) => (
+                <ContactList
+                  key={`${c.name};${c.email}`}
+                  contact={c}
+                  remove={() => deleteContact(`${c.email}`)}
+                />
+              ))}
+            </tbody>
+          </table>
         </div>
       </Layout>
     </div>
