895b7b4c0c8ca24674f87bcb2075bf7568762740
just1ngray <just1ngray@outlook.com>
2020-12-13 17:25:32 -0400

    added credit information at bottom of every page

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,6 @@
 /**
  * This is a react file which will loads first
  *
- * @author:
  * @author Bivash Pandey (A00425523) - Load settings from local storage, apply new theme
  *
  */
@@ -47,18 +46,38 @@ function MyApp({ Component, pageProps }) {
   // Bivash Pandey (A00425523) - End
 
   return (
-    <div style={{ backgroundColor: color, height: "100vh" }}>
+    <div
+      style={{
+        backgroundColor: color,
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
       <link
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css"
         integrity="sha256-2XFplPlrFClt0bIdPgpz8H7ojnk10H69xRqd9+uTShA="
         crossOrigin="anonymous"
       />
-      <section className="section">
+      <section className="section" style={{ flexGrow: 100 }}>
         <div className="container">
           <Component {...pageProps} settings={settings} />
         </div>
       </section>
+
+      <footer className="container" style={{ textAlign: "center" }}>
+        <span
+          onClick={() => window.open("https://github.com/just1ngray/CSCI3428")}
+        >
+          Open Source Software
+        </span>
+        &nbsp;Developed in CSCI 3428 at&nbsp;
+        <span onClick={() => window.open("http://cs.smu.ca")}>
+          Saint Mary's University
+        </span>
+        <p>Fall 2020</p>
+      </footer>
     </div>
   );
 }
