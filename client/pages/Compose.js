57059f348cf19eb7b4d9d6845442086a3fe735f4
Bivash Pandey <36802916+bivashpandey@users.noreply.github.com>
2020-12-14 12:47:26 -0400

    Update Compose.js

--- client/pages/Compose.js
+++ client/pages/Compose.js
@@ -105,7 +105,7 @@ export default function Compose() {
       <br />
       {/*page title*/}
       <div>
-        <PageTitle title= "COMPOSE EMAIL"} />
+        <PageTitle title= "COMPOSE EMAIL" />
       </div>
       <br />
       
